c84b517f3dcad58a6453a5abaf3f86bb2eddcdde
Shuvasis Datta <shuvasisdatta@gmail.com>
2024-10-08 23:07:04 +0300

    Service2-Node.js app source file created after testing

---
+++ Service2/main.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { execSync } = require('child_process');
+
+const app = express();
+
+// Function to get system information
+function getInfo() {
+    // Get the container IP address
+    const ipAddress = execSync("hostname -I").toString().trim();
+    
+    // Get the list of running processes
+    const processes = execSync("ps -ax").toString();
+    
+    // Get available disk space
+    const diskSpace = execSync("df -h /").toString();
+    
+    // Get time since last boot
+    const uptime = execSync("uptime -p").toString().trim();
+    
+    // Return system information
+    return {
+        "IP Address": ipAddress,
+        "Processes": processes,
+        "Disk Space": diskSpace,
+        "Uptime": uptime
+    };
+}
+
+// Endpoint to provide system information
+app.get('/info', (req, res) => {
+    res.json(getInfo());
+});
+
+// Start server on port 5000
+app.listen(5000, () => {
+    console.log('Service2 running on port 5000');
+});
