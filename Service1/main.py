ffa4a337873333574a55c434c95739202ac5ff90
Shuvasis Datta <shuvasisdatta@gmail.com>
2024-11-08 14:20:19 +0200

    bugfix for ps and df cmd

--- Service1/main.py
+++ Service1/main.py
@@ -9,25 +9,30 @@ app = Flask(__name__)
 
 # Function to get system information
 def get_sysinfo():
-    # Get the IP address
-    ip_address = subprocess.getoutput("hostname -I").strip()
-    
-    # Get the list of running processes
-    processes = subprocess.getoutput("ps -ax")
-    
-    # Get available disk space
-    disk_space = subprocess.getoutput("df -h /")
-    
-    # Get time since last boot
-    uptime = subprocess.getoutput("uptime -p")
-    
-    # System information as a dictionary
-    info = {
-        "IP Address": ip_address,
-        "Processes": processes,
-        "Disk Space": disk_space,
-        "Uptime": uptime
-    }
+    try:
+        # Get the IP address
+        ip_address = subprocess.getoutput("hostname -I").strip()
+        
+        # Get the list of running processes
+        processes = subprocess.getoutput("ps -ax")
+        
+        # Get available disk space
+        disk_space = subprocess.getoutput("df -h /")
+        
+        # Get time since last boot
+        uptime = subprocess.getoutput("uptime -p")
+        
+        # System information as a dictionary
+        info = {
+            "IP Address": ip_address,
+            "Processes": processes,
+            "Disk Space": disk_space,
+            "Uptime": uptime
+        }
+    except Exception as e:
+        # In case of any error in retrieving system info, log and return empty info
+        info = {"error": str(e)}
+
     return info
 
 # Endpoint for HTTP requests
