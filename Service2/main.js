192ad6b5ff7299fb6f6a9d0fa908ab522d08dc89
Shuvasis Datta <shuvasisdatta@gmail.com>
2024-11-08 13:58:29 +0200

    added stop route and functionality

--- Service2/main.js
+++ Service2/main.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const { execSync } = require('child_process');
+const Docker = require('dockerode');
 
 const app = express();
+
+// Create Docker instance using the Docker socket
+const docker = new Docker({
+    socketPath: '/var/run/docker.sock'  // Path to the Docker socket inside the container
+  });
 
 // Function to get system information
 function getSysInfo() {
@@ -29,6 +35,24 @@ function getSysInfo() {
 // Endpoint to provide system information
 app.get('/info', (req, res) => {
     res.json(getSysInfo());
+});
+
+
+// Route to handle the shutdown request (called by STOP button)
+app.post('/stop', async (req, res) => {
+    try {
+      // Stop and remove all containers
+      console.log('Stopping and removing containers...');
+      docker.listContainers(function (err, containers) {
+        containers.forEach(function (containerInfo) {
+          docker.getContainer(containerInfo.Id).stop();
+        });
+      });
+      res.send('All containers stopped successfully.');
+    } catch (error) {
+      console.error('Error while stopping containers:', error);
+      res.status(500).send('Failed to stop containers.');
+    }
 });
 
 // Start server on port 5000
